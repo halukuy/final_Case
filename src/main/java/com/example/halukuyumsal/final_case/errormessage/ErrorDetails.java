b4380d5a857507acc586b83955caa5c5da96f2d0
halukuy <halukuyumsal@hotmail.com>
2024-03-15 14:41:52 +0300

    Add ErrorDetails

---
+++ src/main/java/com/example/halukuyumsal/final_case/errormessage/ErrorDetails.java
@@ -0,0 +1,20 @@
+package com.example.halukuyumsal.final_case.errormessage;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+}
